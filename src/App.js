971047b0a37aff144941073159c0732b6ab00ea0
weestoater <ian@weestoater.com>
2019-03-10 11:57:49 +0000

    Clean up and adding pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 
 const DEFAULT_QUERY = 'react';
+const DEFAULT_HPP = '10';
 
 const PATH_BASE = 'http://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
-
-// function isSearched(searchTerm) {
-//   return function (item) {
-//     return item.title.toLowerCase().includes(searchTerm.toLowerCase()) || item.author.toLowerCase().includes(searchTerm.toLowerCase());
-//   }
-// }
+const PARAM_HPP = 'hitsPerPage=';
 
 class App extends Component {
   constructor(props){
@@ -30,11 +25,23 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+    const oldHits = page != 0
+      ? this.state.result.hits
+      : [];
+
+    const updatedHits =[
+      ...oldHits,
+      ...hits
+    ];
+
+    this.setState({ 
+      result: { hits: updatedHits, page} 
+    });
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
-      fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
+      fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
         .then(response => response.json())
         .then(result => this.setSearchTopStories(result))
         .catch(error => error);
@@ -77,11 +84,15 @@ class App extends Component {
 
     return (
       <div className="container-fluid">
-        <Search  value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>Hackernews API</Search>
-        { result && <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} /> }
-        <div className="blah my-4">
-          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page +1)} className="btn btn-info">More articles...</Button>
+        <Search  value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>Hackernews Search</Search>
+        { result && 
+        <div>
+        <Button onClick={() => this.fetchSearchTopStories(searchTerm, page +1)} className="btn btn-info my-4">More articles...</Button>
+        <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} /> 
+        <Button onClick={() => this.fetchSearchTopStories(searchTerm, page +1)} className="btn btn-info my-4">More articles...</Button>
         </div>
+        }
+        
       </div>
     );
   }
@@ -98,7 +109,7 @@ const Search = ({ value, onChange, onSubmit, children }) => {
           <form className="form-inline my-4" onSubmit={onSubmit}>
             <div className="input-group mb-3">
               <div className="input-group-prepend">
-                <label className="input-group-text" for="searchtext">Search for articles about</label>
+                <label className="input-group-text" for="searchtext">Find articles on</label>
               </div>
               <input type="text" className="form-control" onChange={onChange} value={value} 
                 placeholder="Enter filter text here" aria-label="searchTerm" id="searchtext" />
@@ -113,14 +124,14 @@ const Search = ({ value, onChange, onSubmit, children }) => {
   );
 }
  
-
 const Table = ({ list , onDismiss })  => {
   return (
-    <div className="row mt-4">
+    <div className="row my-4">
+      <div className="col-sm-12">No. of articles: <span className="badge badge-info">{list.length}</span> </div>
       {list.map(item =>
         <div key={item.objectID} className="col-sm-3 mb-3">
           <div className="card">
-            <div className="card-header"><a href={item.url}>{item.title}</a></div>
+            <div className="card-header"><a href={item.url} target="_blank" rel="noopener noreferrer">{item.title}</a></div>
             <div className="card-body">
               <p><small className="text-info">Author:</small> { item.author}<br /><small className="text-info">Comments:</small> {item.num_comments} | <small className="text-info">Points:</small> {item.points}</p>
             </div>
