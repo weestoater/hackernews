feacb0864622b229116de7565efa6ae0c44220d0
weestoater <ian@weestoater.com>
2019-03-13 15:44:23 +0000

    Table testing added using Enzyme and shallow.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App, { Search, Button } from './App';
+import Enzyme,  { shallow } from 'enzyme';
+import Adapter from  'enzyme-adapter-react-16';
+import App, { Search, Button, Table } from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('react App -> ', () => {
 
@@ -48,5 +52,24 @@ test('has a valid snapshot', () => {
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+});
+
+describe('Table -> ', () => {
+  
+  const props = {
+    list: [
+      {title: '1', author: '1', num_comments: 1, points: 2, relevancy_score: 123, objectID: 'y'},
+      {title: '2', author: '2', num_comments: 2, points: 4, relevancy_score: 456, objectID: 'z'},
+    ],
+  };
+
+  it('shows two items in a list', () => {
+    const element = shallow(
+      <Table { ...props} />
+    );
+    expect(element.find('.card').length).toBe(2);
+  });
+
 
 });
