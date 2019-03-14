e4a11f8cadfc8dabea9bdfe83a5e76989bb00250
weestoater <ian@weestoater.com>
2019-03-14 15:56:29 +0000

    Refactored out Search tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import Enzyme,  { shallow } from 'enzyme';
 import Adapter from  'enzyme-adapter-react-16';
-import App, { Search, Table } from './App';
+import App, { Table } from './App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -21,22 +21,6 @@ describe('react App -> ', () => {
     expect(tree).toMatchSnapshot();
   });
 
-});
-
-describe('Search -> ', () => {
-
-    it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render( <Search>Hackernews Search</Search> , div);
-      ReactDOM.unmountComponentAtNode(div);
-    });
-
-    test('has a valid snapshot', () => {
-      const component = renderer.create(<Search>Hackernews Search</Search>);
-      const tree = component.toJSON();
-      expect(tree).toMatchSnapshot();
-    });
-  
 });
 
 describe('Table -> ', () => {
